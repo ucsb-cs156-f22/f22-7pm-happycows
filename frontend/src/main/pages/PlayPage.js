af7d187c1f1e23f5a5527b2f507f192858fbd53c
kaiechen <kaiechen@ucsb.edu>
2022-11-22 18:19:16 -0800

    reset

--- frontend/src/main/pages/PlayPage.js
+++ frontend/src/main/pages/PlayPage.js
@@ -82,6 +82,7 @@ export default function PlayPage() {
   // Stryker disable all 
   const mutationbuy = useBackendMutation(
     objectToAxiosParamsBuy,
+    { onSuccess: onSuccessBuy},
     { onError: onErrorBuy}
     // Stryker disable next-line all : hard to set up test for caching
     [`/api/usercommons/forcurrentuser?commonsId=${commonsId}`]
