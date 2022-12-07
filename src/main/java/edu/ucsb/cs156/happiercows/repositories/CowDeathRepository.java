dd46c3285581a94ec305be3f3e360963f831274b
kaiechen <kaiechen@ucsb.edu>
2022-12-07 00:30:25 -0800

    Deleted empty space in CowDeathRepository

--- src/main/java/edu/ucsb/cs156/happiercows/repositories/CowDeathRepository.java
+++ src/main/java/edu/ucsb/cs156/happiercows/repositories/CowDeathRepository.java
@@ -1,16 +1,14 @@
 package edu.ucsb.cs156.happiercows.repositories;
 
 import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import edu.ucsb.cs156.happiercows.entities.CowDeath;
 
 public interface CowDeathRepository extends CrudRepository <CowDeath, Long>{
 
 	Optional<CowDeath> getCowsKilledByCommonsIdAndUserId(Long commons_id, Long user_id );
-	
+
     Iterable<CowDeath> getCowsKilledByCommonsId(Long commons_id);
 }
 
